9531c5cdf6f2bb2564b95c37aa64eb6357d3a0e6
Thomas Hart <thomas.hart@uwaterloo.ca>
2022-03-03 21:26:09 -0800

    Moved button input setup to XML

--- app/src/main/java/com/thomashart/calculatorapp/MainActivity.java
+++ app/src/main/java/com/thomashart/calculatorapp/MainActivity.java
@@ -7,38 +7,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 /**
  * MainActivity class for the calculator app.
  * @author Thomas Hart
  */
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    // Number Buttons
-    Button zeroButton;
-    Button oneButton;
-    Button twoButton;
-    Button threeButton;
-    Button fourButton;
-    Button fiveButton;
-    Button sixButton;
-    Button sevenButton;
-    Button eightButton;
-    Button nineButton;
-
-    // Operator Buttons
-    Button clearButton;
-    Button equalsButton;
-    Button plusButton;
-    Button minusButton;
-    Button multiplyButton;
-    Button divideButton;
-    Button plusMinusButton;
-    Button modulusButton;
-    Button decimalButton;
-
     // TextViews
     TextView calculatorText;
     TextView answerText;
+
+    ArrayList<BigDecimal> inputNums;
 
     /**
      * onCreate: Initializes default application state when activity is created.
@@ -54,54 +36,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         calculatorText.setText("0");
         answerText = (TextView) findViewById(R.id.answer_text);
 
-        initButtons();
-    }
-
-    /**
-     * initButtons: Initializes buttons used on calculator.
-     */
-    private void initButtons() {
-        zeroButton = findViewById(R.id.zero_button);
-        oneButton = findViewById(R.id.one_button);
-        twoButton = findViewById(R.id.two_button);
-        threeButton = findViewById(R.id.three_button);
-        fourButton = findViewById(R.id.four_button);
-        fiveButton = findViewById(R.id.five_button);
-        sixButton = findViewById(R.id.six_button);
-        sevenButton = findViewById(R.id.seven_button);
-        eightButton = findViewById(R.id.eight_button);
-        nineButton = findViewById(R.id.nine_button);
-
-        clearButton = findViewById(R.id.clear_button);
-        equalsButton = findViewById(R.id.equals_button);
-        plusButton = findViewById(R.id.plus_button);
-        minusButton = findViewById(R.id.minus_button);
-        multiplyButton = findViewById(R.id.multiply_button);
-        divideButton = findViewById(R.id.divide_button);
-        plusMinusButton = findViewById(R.id.plus_minus_button);
-        modulusButton = findViewById(R.id.modulus_button);
-        decimalButton = findViewById(R.id.decimal_button);
-
-        zeroButton.setOnClickListener(this);
-        oneButton.setOnClickListener(this);
-        twoButton.setOnClickListener(this);
-        threeButton.setOnClickListener(this);
-        fourButton.setOnClickListener(this);
-        fiveButton.setOnClickListener(this);
-        sixButton.setOnClickListener(this);
-        sevenButton.setOnClickListener(this);
-        eightButton.setOnClickListener(this);
-        nineButton.setOnClickListener(this);
-
-        clearButton.setOnClickListener(this);
-        equalsButton.setOnClickListener(this);
-        plusButton.setOnClickListener(this);
-        minusButton.setOnClickListener(this);
-        multiplyButton.setOnClickListener(this);
-        divideButton.setOnClickListener(this);
-        plusMinusButton.setOnClickListener(this);
-        modulusButton.setOnClickListener(this);
-        decimalButton.setOnClickListener(this);
+        inputNums = new ArrayList<>();
     }
 
     /**
@@ -166,10 +101,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }
     }
 
+    /**
+     * calculate: Performs given calculation based on ArrayLists of numbers and operators.
+     *            Displays result of calculations to screen.
+     */
     private void calculate() {
         answerText.setText(calculatorText.getText());
     }
 
+    /**
+     * clear: Clears any calculations and results currently displayed to the screen.
+     */
     private void clear() {
         calculatorText.setText("0");
         answerText.setText("");
