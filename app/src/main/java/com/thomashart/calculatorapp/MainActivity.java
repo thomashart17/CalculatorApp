dd25b576e53b2b35b9abe69ee2302ef0c742e2d6
Thomas Hart <thomas.hart@uwaterloo.ca>
2022-03-04 19:16:53 -0800

    Replaced switch statement

--- app/src/main/java/com/thomashart/calculatorapp/MainActivity.java
+++ app/src/main/java/com/thomashart/calculatorapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.HapticFeedbackConstants;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
@@ -45,38 +46,8 @@ public class MainActivity extends Activity implements OnCalculatorClickListener 
     @Override
     public void onNumberClick(View view) {
         view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP);
-        switch (view.getId()) {
-            case R.id.zero_button:
-                updateText("0");
-                break;
-            case R.id.one_button:
-                updateText("1");
-                break;
-            case R.id.two_button:
-                updateText("2");
-                break;
-            case R.id.three_button:
-                updateText("3");
-                break;
-            case R.id.four_button:
-                updateText("4");
-                break;
-            case R.id.five_button:
-                updateText("5");
-                break;
-            case R.id.six_button:
-                updateText("6");
-                break;
-            case R.id.seven_button:
-                updateText("7");
-                break;
-            case R.id.eight_button:
-                updateText("8");
-                break;
-            case R.id.nine_button:
-                updateText("9");
-                break;
-        }
+        Button pressedButton = findViewById(view.getId());
+        updateText(pressedButton.getText().toString());
     }
 
     /**
